23b88502272be3c78fabb39d4e4fa17165216e3f
acs101683 <acs106183@gm.ntcu.edu.tw>
2020-09-24 09:56:08 +0800

    add noti

--- notificationManager.py
+++ notificationManager.py
@@ -1,8 +1,13 @@
 import json
 
+try:
+    with open('./notiAc.json') as f:
+            notification_json = json.load(f)
+except IOError as e:
+    notification_json = {'notification': []}
+    with open('./notiAc.json', 'w') as f:
+        json.dump(notification_json, f)
 
-with open('./notiAc.json') as f:
-    notification_json = json.load(f)
 
 def notiPrint():
     return json.dumps(notification_json, indent = 4, sort_keys=True)
